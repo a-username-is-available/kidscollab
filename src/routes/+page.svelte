d311c3fb1c9b0447f1a359d5fc94a70d0da08e7c
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-08 09:19:59 +0000

    Reposition Line

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class='flex flex-col justify-center overflow-x-hidden'>
-    <h1 class='text-black text-9xl font-normal tracking-[-0.6875rem] -translate-y-5'>NameThingy</h1>
+    <h1 class='text-black text-9xl font-normal tracking-[-0.6875rem] -translate-y-5 mt-auto'>NameThingy</h1>
     <h2 class='text-black text-2xl -translate-y-9 translate-x-[0.57rem]'>Smaller Text as Caption</h2>
 
-    <div class='w-96 h-px border-2 border-black item-end'></div>
+    <div class='w-96 h-px border-2 border-black self-end mt-auto mb-5 mr-5'></div>
 </div>
