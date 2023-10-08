e9e139098a42dc15bc0d27db5c2509ceca341346
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-08 09:24:14 +0000

    Change Color

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,5 +1,5 @@
 <span class="h-full pr-[12px] overflow-hidden">
-    <aside class="h-[calc(100%+1.5rem)] bg-stone-100 rounded-3xl border-[5px] border-black">
+    <aside class="h-[calc(100%+1.5rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black">
     
     </aside>
 </span>
