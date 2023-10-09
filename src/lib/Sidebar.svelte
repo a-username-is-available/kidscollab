59b1a60942616c0bfba38a115398f8ca798c7760
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-09 05:17:59 +0000

    Add margin

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -2,9 +2,9 @@
 	import Dropdown from "./Dropdown.svelte";
 </script>
 
-<span class="h-full pr-[12px] overflow-hidden">
+<span class="h-full pr-[12px] overflow-hidden m-1">
     <aside class="h-[calc(100%+1.5rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black">
-        <div class="m-5">
+        <div class="m-4">
             <h2 class="text-black text-2xl font-semibold mb-5">Articles</h2>
             
             <p>
