84e9f1802e7844b14d0db7861ed688893fd0f4c5
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-15 05:56:29 +0000

    Make sidebar sticky

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,40 +1,38 @@
 <script lang='ts'>
 	import Dropdown from "./Dropdown.svelte";
-
-    let width: number = 0;
-    let scrolled: boolean = false;
 </script>
 
-<span bind:clientWidth={width} class="h-full pr-[12px] ml-1">
-    <aside style='width: {width}px;'
-           class="h-[calc(100%+1.5rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black fixed 
-                  {scrolled ? '-translate-y-16' : ''}">
-        <div class="m-4">
-            <h2 class="text-black text-2xl font-semibold">Articles</h2>
-          
-            <Dropdown category='KidsCollab'>
-                <a href="/kidscollab/test_item">Test Item</a>
-            </Dropdown>
-
-            <Dropdown category='Category 2 (dummy)'>
-                <a href="/">Item #1</a>
-                <a href="/">Item with long name that goes to the next line</a>
-                <a href="/">Item #2</a>
-                <a href="/">Item #3</a>
-            </Dropdown>
-
-            <Dropdown category='Category 3 (dummy)'>
-                <a href="/">Item #1</a>
-                <a href="/">Item with long name that goes to the next line</a>
-                <a href="/">Item #2</a>
-                <a href="/">Item #3</a>
-            </Dropdown>
-        </div>
-    </aside>
+<span class="h-[calc(100%+2rem)] overflow-y-clip">
+    <div class="h-[calc(100%+4rem)] pr-[12px] ml-1">
+        <aside class="h-[calc(100vh+1rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black sticky bottom-0 top-4">
+            <div class="m-4">
+                <h2 class="text-black text-2xl font-semibold">Articles</h2>
+    
+                <Dropdown category='KidsCollab'>
+                    <a href="/kidscollab/test_item">Test Item</a>
+                </Dropdown>
+    
+                <Dropdown category='Category 2 (dummy)'>
+                    <a href="/">Item #1</a>
+                    <a href="/">Item with long name that goes to the next line</a>
+                    <a href="/">Item #2</a>
+                    <a href="/">Item #3</a>
+                </Dropdown>
+    
+                <Dropdown category='Category 3 (dummy)'>
+                    <a href="/">Item #1</a>
+                    <a href="/">Item with long name that goes to the next line</a>
+                    <a href="/">Item #2</a>
+                    <a href="/">Item #3</a>
+                </Dropdown>
+            </div>
+        </aside>
+    </div>
 </span>
 
 <style>
     aside {
         box-shadow: 11px 12px 0px 0px #000;
+        clip-path: inset(0 -16px 1.5rem 0);
     }
 </style>
