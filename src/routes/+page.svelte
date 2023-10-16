3dcec49aa5ed1e0b4e988a5528f32dcb9f56429b
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-16 08:18:00 +0000

    Home page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
-<div class='flex flex-col justify-center overflow-x-hidden'>
-    <h1 class='text-black text-9xl font-normal tracking-[-0.6875rem] -translate-y-5 mt-auto'>KidsCollab</h1>
-    <h2 class='text-black text-2xl -translate-y-9 translate-x-[0.57rem]'>Smaller Text as Caption</h2>
+<span>
+    <div class='flex flex-col h-screen justify-center overflow-x-clip'>
+        <h1 class='text-black text-9xl font-normal tracking-[-0.6875rem] -translate-y-5 mt-auto'>KidsCollab</h1>
+        <h4 class='text-black text-2xl -translate-y-9 translate-x-[0.57rem]'>Inspiring Youthful Creativity</h4>
+    
+        <div class='w-96 h-px border-2 border-black self-end mt-auto mb-28 mr-5'></div>
+    </div>
+    
+    <div class='mr-10'>
+        <h2 class="font-bold text-2xl">Acknowledgement Of Country</h2>
+        <p>
+            The creators of this website would like to acknowledge the Aboriginal people of Australia
+            on which land is rightfully the Indigenous people on which we are on today.
+            <br>
+            We pay our respects to the elders, past, present and emerging.
+        </p>
 
-    <div class='w-96 h-px border-2 border-black self-end mt-auto mb-5 mr-5'></div>
-</div>
+        <h2 class="mt-10 font-bold text-2xl">New Items Welcome!</h2>
+        <p>
+            If you have something to share with us on KidsCollab, please email us at kidscollabaus@gmail.com
+        </p>
+    </div>
+</span>
