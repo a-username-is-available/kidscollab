ad94fc776194afad1cfdff0c715c9c68ad918a3c
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-10-29 09:40:38 +0000

    Clean up

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-	import Dropdown from "./Dropdown.svelte";
+	import Accordion from "$lib/Accordion.svelte";
 </script>
 
 <span class="h-[calc(100%+2rem)] overflow-y-clip">
@@ -7,24 +7,6 @@
         <aside class="h-[calc(100vh+1rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black sticky bottom-0 top-4">
             <div class="m-4">
                 <h2 class="text-black text-2xl font-semibold">Articles</h2>
-    
-                <Dropdown category='KidsCollab'>
-                    <a href="/content/kidscollab/test_item">Test Item</a>
-                </Dropdown>
-    
-                <Dropdown category='Category 2 (dummy)'>
-                    <a href="/">Item #1</a>
-                    <a href="/">Item with long name that goes to the next line</a>
-                    <a href="/">Item #2</a>
-                    <a href="/">Item #3</a>
-                </Dropdown>
-    
-                <Dropdown category='Category 3 (dummy)'>
-                    <a href="/">Item #1</a>
-                    <a href="/">Item with long name that goes to the next line</a>
-                    <a href="/">Item #2</a>
-                    <a href="/">Item #3</a>
-                </Dropdown>
             </div>
         </aside>
     </div>
--- src/routes/content/kidscollab/[slug]/+page.server.ts
+++ src/routes/content/kidscollab/[slug]/+page.server.ts
@@ -1,12 +1,12 @@
 const urlPrefix = 
-	'https://raw.githubusercontent.com/a-username-is-available/kidscollab-content/main/dist/kidscollab';
+    'https://raw.githubusercontent.com/a-username-is-available/kidscollab-content/main/dist/kidscollab';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	// if (params.slug.includes('..')) throw Error('slash cannot be in slug')
+    // if (params.slug.includes('..')) throw Error('slash cannot be in slug')
 
-	const content = await (await fetch(`${urlPrefix}/${params.slug}`)).text()
-	// console.log(content)
+    const content = await (await fetch(`${urlPrefix}/${params.slug}`)).text()
+    // console.log(content)
 
     return {
         content
