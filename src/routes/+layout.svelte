083d9f9837f6d694a97d3e7c07ba4a824826de34
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-11-01 08:26:18 +0000

    Implement pop-up menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,20 @@
 
 	export let data;
 	let shown = false;
+
+	function toggleMenu() {
+		shown = !shown;
+	}
 </script>
 
 <main class='h-full flex flex-col lg:text-base text-lg'>
-	<Navbar />
+	<Navbar on:toggleMenu={toggleMenu}/>
 	<div class='flex lg:justify-start justify-center gap-20 m-2 mb-0 h-max basis-5'>
-		<div class='shrink-0 w-[max(20rem,26%)] overflow-y-clip lg:block hidden'> <Sidebar {data}/> </div>
+		<div class='shrink-0 overflow-y-clip lg:m-0 lg:w-[max(20rem,26%)] lg:block lg:static
+		  absolute w-1/2 transition {shown ? 'md-opacity-100' : 'md:opacity-0 md:translate-y-5'}'>
+			<Sidebar {data}/>
+		</div>
+
 		<div class="mb-2 lg:m-0 md:ml-20 md:mr-20 ml-2 mr-2"> <slot /> </div>
 	</div>
 </main>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,10 +1,28 @@
-<nav class='flex p-5 items-center mr-2 ml-2'>
+<script>
+	import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function toggleMenu() {
+       dispatch('toggleMenu');
+    }
+</script>
+
+<nav class='flex gap-3 p-5 items-center mr-2 ml-2'>
+    <button class='lg:hidden inline-block' on:click={toggleMenu}>
+        <svg viewBox="0 0 55 47" width="20" height="20">
+            <rect rx="5" y="0"  width="55" height="7"></rect>
+            <rect rx="5" y="20" width="35" height="7"></rect>
+            <rect rx="5" y="40" width="45" height="7"></rect>
+        </svg>
+    </button>    
+
     <p class="text-black text-2xl font-semibold">LogoThingy</p>
 
     <!-- space -->
     <div class="flex-grow"></div>
 
-    <div class="text-right text-black text-md font-normal flex gap-10">
+    <div class="text-right text-black font-normal flex gap-10">
         <a href="/">home</a>
         <a href="/about">about</a>
         <a href="/">idk what to put here</a>
