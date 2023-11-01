ac19e01bd7419105500bca30301850d933c528d6
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-11-01 09:55:52 +0000

    Animate hamburger icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,8 +14,8 @@
 <main class='h-full flex flex-col lg:text-base text-lg'>
 	<Navbar on:toggleMenu={toggleMenu}/>
 	<div class='flex lg:justify-start justify-center gap-20 m-2 mb-0 h-max basis-5'>
-		<div class='shrink-0 overflow-y-clip lg:m-0 lg:w-[max(20rem,26%)] lg:block lg:static
-		  absolute w-1/2 transition {shown ? 'md-opacity-100' : 'md:opacity-0 md:translate-y-5'}'>
+		<div class='shrink-0 overflow-y-clip lg:m-0 lg:w-[max(20rem,26%)] lg:block lg:static lg:opacity-100 lg:translate-y-0
+		  absolute w-1/2 transition {shown ? '' : 'opacity-0 translate-y-5'}'>
 			<Sidebar {data}/>
 		</div>
 
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,18 +2,20 @@
 	import { createEventDispatcher } from "svelte";
 
     const dispatch = createEventDispatcher();
+    let open = false;
 
     function toggleMenu() {
        dispatch('toggleMenu');
+       open = !open;
     }
 </script>
 
 <nav class='flex gap-3 p-5 items-center mr-2 ml-2'>
-    <button class='lg:hidden inline-block' on:click={toggleMenu}>
+    <button class='lg:hidden inline-block {open ? 'open' : ''}' on:click={toggleMenu}>
         <svg viewBox="0 0 55 47" width="20" height="20">
-            <rect rx="5" y="0"  width="55" height="7"></rect>
-            <rect rx="5" y="20" width="35" height="7"></rect>
-            <rect rx="5" y="40" width="45" height="7"></rect>
+            <rect class="top" rx="5" y="0" width="55" height="7"></rect>
+            <rect class="middle" rx="5" y="20" width="55" height="7"></rect>
+            <rect class="bottom" rx="5" y="40" width="55" height="7"></rect>
         </svg>
     </button>    
 
@@ -28,3 +30,26 @@
         <a href="/">idk what to put here</a>
     </div>
 </nav>
+
+<style>
+    .open .top {
+        transform: translate(-13.75px, 13px) rotate(45deg);
+    }
+
+    .open .middle {
+        opacity: 0;
+    }
+    
+    .open .bottom {
+        transform: translate(-13.75px, -13px) rotate(-45deg);
+    }
+
+    .top, .bottom {
+        transition: transform 250ms ease-in-out;
+        transform-origin: center;
+    }
+    
+    rect {
+        transition: opacity 250ms ease-in-out;
+    }
+</style>
