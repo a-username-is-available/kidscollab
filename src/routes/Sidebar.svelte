a3c780fb43c304d36412782143a985e20879c476
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-11-04 05:28:15 +0000

    Sidebar scrolls if needs to

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -9,48 +9,44 @@
     const fixName = (str: string) => str.replaceAll(' ', '-').replaceAll(/[#?]/g, '');
 </script>
 
-<div id='floating-panel' class="h-[calc(100%+4rem)] pr-[12px] ml-1">
+<div id='sticky-area' class="h-[calc(100%+2rem)] pr-[12px] ml-1">
     <aside 
         class="h-[calc(100vh+1rem)] bg-[#F5EEE9] rounded-3xl border-[5px] border-black 
         sticky bottom-0 top-4 p-4">
         <h2 class="text-black text-2xl mb-2 font-semibold">Articles</h2>
-        <div class="flex flex-col mb-1 md:hidden ">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+        <div class="overflow-y-auto lg:h-[calc(100%-5rem)] h-[calc(100%-8rem)]">
+            <div class="flex flex-col mb-1 md:hidden ">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </div>
+            {#each data.nav as file}
+            {#if typeof file === 'string'}
+                <a href='/content/{fixName(file)}' >{file}</a>
+            {:else}
+                <Accordion category={file.name}>
+                    {#each file.content as innerFile}
+                    {#if typeof innerFile === 'string'}
+                        <a href='/content/{fixName(file.name)}/{fixName(innerFile)}'>{innerFile}</a>
+                    {:else}
+                        <Accordion category={innerFile.name}>
+                            {#each innerFile.content as innerInnerFile}
+                            <a href='/content/{fixName(file.name)}/{fixName(innerFile.name)}/{fixName(innerInnerFile)}' >
+                                {innerInnerFile}
+                            </a>
+                            {/each}
+                        </Accordion>
+                    {/if}
+                    {/each}
+                </Accordion>
+            {/if}
+            {/each}
         </div>
-
-        {#each data.nav as file}
-        {#if typeof file === 'string'}
-            <a href='/content/{fixName(file)}' >{file}</a>
-        {:else}
-            <Accordion category={file.name}>
-                {#each file.content as innerFile}
-                {#if typeof innerFile === 'string'}
-                    <a href='/content/{fixName(file.name)}/{fixName(innerFile)}'>{innerFile}</a>
-                {:else}
-                    <Accordion category={innerFile.name}>
-                        {#each innerFile.content as innerInnerFile}
-                        <a href='/content/{fixName(file.name)}/{fixName(innerFile.name)}/{fixName(innerInnerFile)}' >
-                            {innerInnerFile}
-                        </a>
-                        {/each}
-                    </Accordion>
-                {/if}
-                {/each}
-            </Accordion>
-        {/if}
-        {/each}
     </aside>
 </div>
 
 <style>
     aside {
         box-shadow: 11px 12px 0px 0px #000;
-        clip-path: inset(0 -16px 1.5rem 0);
-    }
-
-    #floating-panel {
-        clip-path: inset(0 0 1.5rem 0);
     }
 
     a {
