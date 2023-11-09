f8e10a1c4e0923d1bd2165379b874974bf162a68
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-11-09 10:51:57 +0000

    Update LinkedAccordion for new structure

--- src/lib/LinkedAccordion.svelte
+++ src/lib/LinkedAccordion.svelte
@@ -1,15 +1,27 @@
 <script lang='ts'>
 	import Accordion from "./Accordion.svelte";
 
-    export let data: any;
+    type file = {
+        uuid: string,
+        name: string,
+    };
+
+    type category = {
+        name: string,
+        content: item[],
+    };
+
+    type item = file | category;
+
+    export let data: item[];
     export let pathPrefix: string[] = [];
 
     const fixName = (str: string) => str.replaceAll(' ', '-').replaceAll(/[#?]/g, '');
 </script>
 
 {#each data as file}
-    {#if typeof file === 'string'}
-        <a class='block w-max mt-1' href='/content/{pathPrefix.join('/')}/{fixName(file)}'>{file}</a>
+    {#if 'uuid' in file}
+        <a class='block w-max mt-1' href='/content/{pathPrefix.join('/')}/{fixName(file.name)}'>{file.name}</a>
     {:else}
         <Accordion category={file.name}>
             <svelte:self pathPrefix={[...pathPrefix, fixName(file.name)]} data={file.content}/>
