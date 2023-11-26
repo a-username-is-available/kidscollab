0ad4d589b67db9e3df4d462e979f4fd04548fdc7
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2023-11-26 05:05:52 +0000

    Add license notice for CC BY

--- src/routes/content/[...file]/Like.svelte
+++ src/routes/content/[...file]/Like.svelte
@@ -56,7 +56,7 @@
 </script>
 
 <Button type="outline" click={toggle}>
-    <!-- Switch for better icons later -->
+    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
         {#if liked}
             <path
